2b2aad3ea55ce326424ccc08f66087c005876e38
Andre <andrecooke@hotmail.com>
2023-06-15 22:45:04 -0400

    Commit initial

---
+++ MouflonsHardcoreMod/JumpRestriction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Vintagestory.API;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using System.Windows;
+using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+
+namespace MouflonsHardcoreMod 
+{
+    public class JumpRestriction : BlockBehavior
+    {
+        public JumpRestriction(Block block) : base(block)
+        {
+
+        }
+
+        public override bool TryPlaceBlock(IWorldAccessor world, IPlayer byPlayer, ItemStack itemstack, BlockSelection blockSel, ref EnumHandling handling, ref string failureCode)
+         {
+            handling = EnumHandling.PreventDefault;
+
+            if (byPlayer.Entity.Controls.Jump)
+             {
+                failureCode = " Non Non faut pas sauter";
+                return false;
+            }
+
+            return base.TryPlaceBlock(world, byPlayer, itemstack, blockSel, ref handling, ref failureCode);
+        }
+    }
+}
+
+
---
+++ MouflonsHardcoreMod/MouflonsHardcoreMod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Vintagestory.API;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+using Vintagestory.API.Config;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace MouflonsHardcoreMod
+{
+    public class Core : ModSystem
+    {
+        ICoreAPI api;
+        List<string> lesblocs = new List<string> { "packeddirt", "aridpackeddirt", "etc" };
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            this.api = api;
+            this.api.RegisterBlockBehaviorClass("JumpRestriction", typeof(JumpRestriction));
+
+            this.api.World.Logger.Event("started 'Les Mouflons sont Hardcore' mod");
+            this.api.World.Logger.StoryEvent(Lang.Get("Les Mouflons sont Hardcore...", Array.Empty<object>()));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+            api.World.Logger.Event("started 'Les Mouflons sont Hardcore' mod");
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            api.World.Logger.Event("started 'Les Mouflons sont Hardcore' mod");
+        }
+        
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            foreach (var block in api.World.Blocks)
+            {
+                
+                if (block.BlockMaterial.ToString() == "Soil")
+                {
+                    //MessageBox.Show(block.Code.GetName());
+                    block.BlockBehaviors = block.BlockBehaviors.Append(new JumpRestriction(block));
+                }
+            }
+        }
+    }
+}
